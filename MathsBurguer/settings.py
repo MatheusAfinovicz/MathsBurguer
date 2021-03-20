2ae6502ec227024615c643f994dc309ab7aa26a9
Matheus <matheusafinovicz@gmail.com>
2021-03-20 20:37:10 -0300

    Time zone and language

--- MathsBurguer/settings.py
+++ MathsBurguer/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'MathsBurguer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
