5555291dacfc363fc6391345ec5152d687a4dff1
Matheus <matheusafinovicz@gmail.com>
2021-03-21 04:52:58 -0300

    django messages added

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import FormMessageBox
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+from django.contrib import messages
 
 
 def index(request):
@@ -16,6 +17,7 @@ def home(request):
     form = FormMessageBox(request.POST)
 
     if not form.is_valid():
+        messages.add_message(request, messages.ERROR, 'Algo não é valido')
         form = FormMessageBox(request.POST)
         return render(request, 'landingPage.html', {'form': form})
 
@@ -25,8 +27,10 @@ def home(request):
         validate_email(email)
 
     except ValidationError:
+        messages.add_message(request, messages.ERROR, 'E-mail inválido')
         form = FormMessageBox(request.POST)
         return render(request, 'landingPage.html', {'form': form})
 
     form.save()
+    messages.add_message(request, messages.SUCCESS, 'Recebemos sua mensagem!')
     return redirect('/home/')
